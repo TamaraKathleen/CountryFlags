69b9bdbd0345f63d305222a117dccd99fc12d096
Tamara Kathleen <tkathleen@ciandt.com>
2022-06-27 13:39:47 -0300

    implementation of the flags quiz app.

---
+++ app/src/main/java/eu/android/myquizapp/ResultActivity.kt
@@ -0,0 +1,31 @@
+package eu.android.myquizapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        val tvName: TextView = findViewById(R.id.tv_name)
+        val tvScore:TextView = findViewById(R.id.tv_score)
+        val btnFinish: Button = findViewById(R.id.btn_finish)
+
+        val userName = intent.getStringExtra(Constants.USER_NAME)
+        tvName.text = userName
+
+        val totalQuestions = intent.getIntExtra(Constants.TOTAL_QUESTIONS, 0)
+        val correctAnswers = intent.getIntExtra(Constants.CORRECT_ANSWERS, 0)
+
+        tvScore.text = "Your Score is $correctAnswers out of $totalQuestions."
+
+        btnFinish.setOnClickListener {
+            //
+            startActivity(Intent(this@ResultActivity, MainActivity::class.java))
+        }
+    }
+}
